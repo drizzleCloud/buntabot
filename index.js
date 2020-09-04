fb3bf499091e68752a52bf70471f871085fc73da
drizzleCloud <42846533+drizzleCloud@users.noreply.github.com>
2020-09-04 13:24:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const client = new Discord.Client();
-const fs = require('fs');
-
 const prefix = "!";
 
 client.on("message", function(message) {
@@ -16,65 +14,87 @@ client.on("message", function(message) {
 
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`); 
   }
-
-  else if (command === "sum") {
-    const numArgs = args.map(x => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => counter += x);
-    message.reply(`The sum of all the arguments you provided is ${sum}!`);
-  }
-
+  
   else if (command === "avatar") {
-      message.reply(message.author.displayAvatarURL({dynamic:true,size:4096}));
+    message.reply(message.author.displayAvatarURL({dynamic:true,size:4096}));
   }  
 
   else if (command === "smile") {
-    const attachment = new Discord.MessageAttachment('https://i.pinimg.com/originals/0d/b5/eb/0db5eb21866e4e3063a2e2c85ca4e77c.jpg');
-    message.channel.send(attachment);
+    const attachment = new Discord.MessageAttachment(
+      'https://cdn.discordapp.com/attachments/750847967430443050/751468088305909841/0db5eb21866e4e3063a2e2c85ca4e77c.jpg');
+    message.reply(attachment);
   }  
 
-  else if (command === "smug") {
-    const attachment = new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/457008618110451722/745677591855366174/eeveewatersmug.png');
-    message.channel.send(attachment);
+  else if (command === "ps5") {
+    const attachment = new Discord.MessageAttachment(
+      'https://cdn.discordapp.com/attachments/750847967430443050/751469661924884561/tenor.gif');
+    message.reply(attachment);
   }  
 
+  else if (command == 'ht') {
+    const acceptedReplies = ['h', 't'];
+    const random = Math.floor((Math.random() * acceptedReplies.length));
+    const result = acceptedReplies[random];
+    const choice = args[0];
+    const hcembed = {
+      color: 7584788,
+      description: 'Correct',
+      image: {
+        url: 'https://cdn.discordapp.com/attachments/750847967430443050/751493440193167431/heads.png',
+      },
+    };
+    const hwembed = {
+      color: 7584788,
+      description: 'Wrong',
+      image: {
+        url: 'https://cdn.discordapp.com/attachments/750847967430443050/751493440193167431/heads.png',
+      },
+    };
+    const tcembed = {
+      color: 7584788,
+      description: 'Correct',
+      image: {
+        url: 'https://cdn.discordapp.com/attachments/750847967430443050/751493350548307988/tails.png',
+      },
+    };
+    const twembed = {
+      color: 7584788,
+      description: 'Wrong',
+      image: {
+        url: 'https://cdn.discordapp.com/attachments/750847967430443050/751493350548307988/tails.png',
+      },
+    };
 
-
-  else if (command === "gorilla") {
-    const attachment = new Discord.MessageAttachment('https://dpatlarge.files.wordpress.com/2013/10/just-gorillas-havin-fun.gif?w=584');
-    message.channel.send(attachment);
-  }  
-
-  else if (message.content === '!memes') {
-    // Get the buffer from the 'memes.txt', assuming that the file exists
-    const buffer = fs.readFileSync('./memes.txt');
-
-    /**
-     * Create the attachment using MessageAttachment,
-     * overwritting the default file name to 'memes.txt'
-     * Read more about it over at
-     * http://discord.js.org/#/docs/main/master/class/MessageAttachment
-     */
-    const attachment = new Discord.MessageAttachment(buffer, 'memes.txt');
-    // Send the attachment in the message channel with a content
-    message.channel.send(`${message.author}, here are your memes!`, attachment);
+    if (!choice) return message.channel.send(`How to play: \`${prefix}bf <h> Heads, <t> Tails\``);
+    if (!acceptedReplies.includes(choice)) return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
+    
+    if (result == choice) { //correct
+      if (result == 'h'){ //heads
+          message.channel.send({ embed: hcembed });
+      }
+      else if (result == 't'){ //tails
+          message.channel.send({ embed: tcembed });
+      }
+    }
+    else if (result != choice){ //wrong
+      if (result == 'h'){ //heads
+          message.channel.send({ embed: hwembed });
+      }
+      else if (result == 't'){ //tails
+          message.channel.send({ embed: twembed });
+      }
+    }
   }
-
-
-  else if (command === "help") {
-    message.reply(`
-    !ping: returns ping
-    !sum: sums up numbers provided
-    !avatar: shows big avatar
-    !smile: >:)
-    !smug: water stone
-    !help: display all current commmands
-    gorilla`);
-  }
-
+  
   else {
-    message.reply(`For all commands type !help`);
+    message.reply(`
+    !ping
+    !avatar
+    !smile
+    !ps5
+    !ht`);
   }
 });
 
